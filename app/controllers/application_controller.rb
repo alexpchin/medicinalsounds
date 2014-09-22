7371b33763c1160dc1ee5c768b19d6d914587361
alexpchin <alex@womapp.com>
2014-09-22 21:35:09 +0100

    Adding some styling to the site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:account_update) { |u| u.permit!}
-    end
+      devise_parameter_sanitizer.for(:sign_up) << [:firstname, :lastname, :image, :remote_image_url, :username]
+      devise_parameter_sanitizer.for(:account_update) << [:firstname, :lastname, :image, :remote_image_url, :username]
+    end 
 end
